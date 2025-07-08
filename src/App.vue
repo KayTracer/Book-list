84031747b4ffa93dc7b154eaba2d6c0800de4178
KayTracer <cainaluz@gmail.com>
2025-07-08 19:49:15 -0300

    Testei como funciona a importação de componentes usando o Composition API, alem das props e computed props.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Progress from "./components/Progress.vue";
+import { ref, reactive } from "vue";
+
+let courses = reactive([
+  {
+    title: "Tantra",
+    done: true,
+  },
+  {
+    title: "Vue.js",
+    done: true,
+  },
+  {
+    title: "Python",
+    done: false,
+  },
+]);
+
+let newCourse = { done: false };
+
+function addCourse() {
+  courses.push(newCourse);
+  newCourse = { done: false };
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Lista de Cursos</h1>
+    <ul>
+      <li v-for="course in courses" :key="course.title">{{ course.title }}</li>
+    </ul>
+    <input type="text" v-model="newCourse.title" />
+    <button @click="addCourse()">Add new Course</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Progress :courses="courses" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/Progress.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { computed } from "vue";
+const props = defineProps(["courses"]);
+
+const coursesDone = computed(() => {
+  return props.courses.filter((course) => course.done);
+});
+</script>
+
+<template>
+  <div>
+    {{
+      `${coursesDone.length} cursos concluídos de um total de: ${courses.length}`
+    }}
+  </div>
+</template>
