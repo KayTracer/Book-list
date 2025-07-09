f1f01cc3898325b41b95975c249b7d114cfae283
KayTracer <cainaluz@gmail.com>
2025-07-09 14:56:27 -0300

    Adicionei os 3 componentes principais do projeto e modifiquei o style.css com o estilo proposto e o index.html para pegar os ícones necessários do google. Aqui eu utilizei bastante o  para conseguir ativar funções do objeto pai pelo filho, além de ter usado bastante computed properties também. Próximo passo é atualizar o próximo id disponível ao adicionar um novo livro pelo front-ending mesmo, pois não temos o backend, onde seria o normal a se fazer.

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,79 @@
 <script setup>
-import Progress from "./components/Progress.vue";
-import { ref, reactive } from "vue";
+import { reactive, ref } from "vue";
+import Books from "./components/Books.vue";
+import BookProgress from "./components/BookProgress.vue";
+import AddBook from "./components/AddBook.vue";
 
-let courses = reactive([
+let books = reactive([
   {
-    title: "Tantra",
-    done: true,
+    id: 1,
+    title: "History of Europe",
+    cover:
+      "https://www.presenca.pt/cdn/shop/products/image-1_c64f337f-8164-430c-b770-ad582280f726_grande.jpg?v=1607092126",
+    isRead: true,
+    isbn: "0-395-07157-8",
+    author: "Daniel Trejo",
   },
   {
-    title: "Vue.js",
-    done: true,
+    id: 2,
+    title: "Penguin Classics",
+    cover: "https://m.media-amazon.com/images/I/91cKrZxBuwL.jpg",
+    isRead: false,
+    isbn: "0-395-07157-8",
+    author: "Daniel Trejo, Jon Snow",
   },
   {
-    title: "Python",
-    done: false,
+    id: 3,
+    title: "Becoming",
+    cover: "https://images-us.bookshop.org/ingram/9784087861174.jpg?v=enc-v1",
+    isRead: false,
+    isbn: "0-395-07157-8",
+    author: "Daniel Trejo",
+  },
+  {
+    id: 4,
+    title: "Death Stranding 2: On the Beach",
+    cover:
+      "https://m.media-amazon.com/images/M/MV5BYmVkNDM3MzUtN2YyYS00MDg0LWIxN2EtMjQ2ZGQ1ZDhlZWY4XkEyXkFqcGc@._V1_.jpg",
+    isRead: false,
+    isbn: "0-395-07157-8",
+    author: "Kojima-san",
   },
 ]);
 
-let newCourse = { done: false };
-
-function addCourse() {
-  courses.push(newCourse);
-  newCourse = { done: false };
+function addBook(newBook) {
+  books.push(newBook);
+  showAddBook.value = false;
 }
+
+function toggleisRead(id) {
+  books.forEach((book) => {
+    if (book.id === id) {
+      book.isRead = !book.isRead;
+    }
+  });
+}
+
+let showAddBook = ref(false);
 </script>
 
 <template>
-  <div>
-    <h1>Lista de Cursos</h1>
-    <ul>
-      <li v-for="course in courses" :key="course.title">{{ course.title }}</li>
-    </ul>
-    <input type="text" v-model="newCourse.title" />
-    <button @click="addCourse()">Add new Course</button>
+  <div v-if="!showAddBook" class="container">
+    <h1>📖 Meus Livros</h1>
+    <div class="header-btns">
+      <button class="btn" @click="showAddBook = !showAddBook">
+        Adicionar Livro +
+      </button>
+    </div>
+
+    <div class="books-container">
+      <Books @toggleisRead="toggleisRead" :books="books" />
+    </div>
+    <BookProgress :books="books" />
   </div>
-  <Progress :courses="courses" />
+  <div v-else class="container">
+    <AddBook @addBook="addBook" @closeAddBook="showAddBook = false" />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/BookProgress.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps(["books"]);
+
+const booksRead = computed(() => {
+  return props.books.filter((book) => book.isRead).length;
+});
+
+const message = computed(() => {
+  return booksRead.value >= props.books.length
+    ? "Parabéns, você leu todos!"
+    : `${booksRead.value} de ${props.books.length} livros lidos`;
+});
+</script>
+
+<template>
+  <div class="books-read">
+    <label for="progress">Seu progresso</label><br />
+    <progress :value="booksRead" :max="books.length"></progress>
+    <p>{{ message }}</p>
+  </div>
+</template>
